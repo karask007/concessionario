340b8f803a713fd6d0fd733296dbdc3896e4aad3
afren <afren@LAPTOP-88R6BNSD.fritz.box>
2021-07-16 14:48:55 +0200

    ff

--- concessionarioBack/src/concessionarioBack/controller/service/Connect.java
+++ concessionarioBack/src/concessionarioBack/controller/service/Connect.java
@@ -1,5 +1,11 @@
 package concessionarioBack.controller.service;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Connect {
+	URL url = new URL("http://example.com");
+	HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	con.setRequestMethod("POST");
 
 }
